e3bd77558187e7d8ac78cdfc1160a845d9c75b07
moltobennne <155998347+moltobennne@users.noreply.github.com>
2024-04-29 15:30:48 +0200

    metodos para revisar el nif y el cif de los clientes y proveedores

---
+++ contabilidadNuevo/contabilidadNuevo/src/main/java/com/mycompany/contabilidad/objects/Proveedor.java
@@ -0,0 +1,24 @@
+package com.mycompany.contabilidad.objects;
+
+/**
+ *
+ * @author alex
+ */
+public class Proveedor extends Usuario {
+    
+    private String nombreEmpresa;
+
+    public Proveedor(String nombreEmpresa, String nombreContacto, String id, String address, int telephone, String email) {
+        super(nombreContacto, id, address, telephone, email);
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+       
+}
