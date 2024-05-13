c7856311c185d016191e82b2e74ad37978d4b0af
moltobennne <155998347+moltobennne@users.noreply.github.com>
2024-05-13 15:51:11 +0200

    DAO hecho

---
+++ proyectoContabilidadDAM-marc/contabilidadNuevo/src/main/java/com/mycompany/contabilidad/objects/Cliente.java
@@ -0,0 +1,27 @@
+package com.mycompany.contabilidad.objects;
+
+/**
+ *
+ * @author alex
+ */
+public class Cliente extends Usuario {
+    
+    private String subcomercial;
+
+    public Cliente(String subcomercial, String nombreContacto, String id, String address, String telephone, String email) {
+        super(nombreContacto, id, address, telephone, email);
+        this.subcomercial = subcomercial;
+    }
+
+    public String getSubcomercial() {
+        return subcomercial;
+    }
+
+    public void setSubcomercial(String subcomercial) {
+        this.subcomercial = subcomercial;
+    }
+    
+    
+    
+    
+}
